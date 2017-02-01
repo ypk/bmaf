120fccbbd08a6d07f4df026159d170995b063ddd
Prasanna K Yalala <yprasannakumar@gmail.com>
2017-02-01 13:22:21 +0000

    working httpservice

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,6 +1,8 @@
 class HomeController {
-  constructor() {
+  constructor(httpService) {
     this.name = 'home';
+
+    httpService.getData();
   }
 }
 
--- src/app/services/http-service.js
+++ src/app/services/http-service.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 export class HttpService {
     constructor() {
         this.name = "HttpService";
-        axios.get("//jsonplaceholder.typicode.com/posts/1").then(function(data) {
-            console.log(data);
-        }).catch(function(error) {
-            console.log(error);
-        });
+    }
+    getData(){
+      axios.get("//jsonplaceholder.typicode.com/posts/1").then(function(data) {
+          console.log(data);
+      }).catch(function(error) {
+          console.log(error);
+      });
     }
 }
